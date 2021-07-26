37d2ec5fc5c2d190a7d6db1eb12bd1b2e8dcc385
Dat Phan <ptd0301@gmail.com>
2021-07-26 19:47:45 +0700

    update lan 1

--- index.js
+++
@@ -1,5 +0,0 @@
-const express = require('express'),
-    app = express(),
-    port = 3000;
-app.use(express.static(__dirname + '/public'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
